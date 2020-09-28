f21160748c4d5c478f61d7eaa0e0979d6b2b1001
Kun Patrik <kunpatrik00@gmail.com>
2020-09-28 18:11:02 +0200

    Drill 3 done

--- source/Ch03/drill/letter.cpp
+++ source/Ch03/drill/letter.cpp
@@ -5,9 +5,48 @@ int main()
 	string first_name;
 	cout << "Enter the name of the person you want to write to: ";
 	cin >> first_name;
-	cout << "Dear ";
-	cout << first_name;
-	cout << ", \nHow are you? I am fine. I miss you. \n";
+	cout << "Dear " << first_name << ",\nHow are you? I am fine. I miss you. \n";
 
+	string friend_name;
+	char friend_sex = 0;
+
+	cout << "Enter the name and sex ('f' or 'm') of your friend divided by a whitespace: ";
+	cin >> friend_name >> friend_sex;
+
+	cout << "Have you seen " << friend_name << " lately?";
+
+	if (friend_sex == 'm')
+	{
+		cout << "\nIf you see " + friend_name + " please ask him to call me.";
+	}
+	else if (friend_sex == 'f')
+	{
+		cout << "\nIf you see " + friend_name + " please ask her to call me.";
+	}
+
+	int age;
+	cout << "\nEnter the age of your friend: ";
+	cin >> age;
+	if (age <= 0 || age > 110)
+	{
+		simple_error("You are kidding!");
+	}
+	else if (age < 12)
+	{
+		cout << "Next year you will be ";
+		cout << age + 1;
+		cout << '!';
+	}
+	else if (age == 17)
+	{
+		cout << "Next year you will be able to vote!";
+	}
+	else if (age > 70)
+	{
+		cout << "I hope you are enjoying retirement!";
+	}
+
+	
+	cout << "\nYours sincerely,\n" << endl << endl;
 	return 0;
 }
